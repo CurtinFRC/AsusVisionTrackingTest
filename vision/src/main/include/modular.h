6a776fee5bf067b833468d22e773dfd66331ed93
CJBuchel <connorbuchel@gmail.com>
2019-03-22 14:44:20 +0800

    Updated License and modular file

---
+++ vision/src/main/include/modular.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <opencv2/core/core.hpp>
+#include <cscore.h>
+
+#include "threading/Runnable.h"
+
+#include <mutex>
+
+class Modularity {
+ public:
+  virtual void GetDisplayMat(cv::Mat &displayMat) = 0;
+  virtual cv::Size GetDisplaySize() = 0;
+};
+
+class Modular : public RunModular {
+ public:
+
+  void Init() override;
+  void Periodic() override;
+
+ private:
+  std::string _name;
+  Modularity &_target;
+  
+  cs::CvSource _output;
+  
+  cv::Mat _displayMat;
+
+  bool UseKinect;
+  bool DriverCam;
+
+  int CamPorts_W2 = (DetectCam, DriverCam);
+  int CamProts_W1 = (DetectCam);
+
+
+  bool DetectRound;
+  bool DetectSquare;
+
+  bool red;
+  bool green;
+  bool blue;
+
+}
