097873236688ae8e2895723243659c8247fa9cfe
CJBuchel <connorbuchel@gmail.com>
2019-04-06 13:09:43 +0800

    Added recourses for GUI

--- Vision_GUI/Form1.cs
+++ Vision_GUI/Form1.cs
@@ -10,11 +10,62 @@ using System.Windows.Forms;
 
 namespace Vision_GUI
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "vision")
+            {
+                if (textBox4.Text == "curtinfrc")
+                {
+                    this.Hide();
+                    Form2 ss = new Form2();
+                    ss.Show();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Username Or Password Is Incorrect");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox4.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBox4.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                textBox4.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBox4.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
