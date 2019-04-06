eeb7ed2a5e9b543b9ade8a76b76fe5879d12c8fc
CJBuchel <connorbuchel@gmail.com>
2019-04-06 14:30:30 +0800

    GUI can now see local camera streams

--- Vision_GUI/Form2.Designer.cs
+++ Vision_GUI/Form2.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,6 +44,27 @@
             this.pictureBox1.Size = new System.Drawing.Size(525, 335);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "TinkerBoard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "LocalHost";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form2
             // 
@@ -51,9 +74,12 @@
             this.BackgroundImage = global::Vision_GUI.Properties.Resources.the_next_bing_thing;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1023, 634);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form2";
             this.Text = "Vision Tracking";
+            this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -62,5 +88,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
