5e0edaa663a6f3264429fdaa88569c37ad6f459e
CJBuchel <connorbuchel@gmail.com>
2019-05-06 17:27:03 +0800

    Started linking variables in visionMap
    Completed camport exposure, (the basic stuff)

--- vision/src/main/include/visionMap.h
+++ vision/src/main/include/visionMap.h
@@ -4,8 +4,29 @@
 void visionMapInit();
 void visionMapPeriodic();
 
+bool usingKinect;
+int camPort;
+int resWidth;
+int resHeight;
+int camExposure;
+std::string colour;
+int Rvalue;
+int Gvalue;
+int Bvalue;
+bool useTapeDetection;
+bool useBallDetection;
+bool personDetection;
+bool CustomDetection;
+bool offset;
+bool position;
+bool distance;
+int blur;
+int noisefix;
+
+
 class VisionMap {
  public:
+
 
  private:
 
